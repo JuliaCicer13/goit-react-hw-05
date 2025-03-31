6538492ec145d7bbf8ad24fa3360c5bb2a54e586
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-03-31 23:59:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@
 import css from './App.module.css';
 import { Route, Routes, Link } from "react-router-dom";
 import Home from "path/to/pages/Home";
-import About from "path/to/pages/About";
+import About from ".path/to/pages/About";
 import Products from "path/to/pages/Products";
 import NotFound from "path/to/pages/NotFound";
 import BlogPost from "path/to/pages/BlogPost";
-import ProductDetails from "path/to/pages/ProductDetails";
+import ProductDetails from ".path/to/pages/ProductDetails";
 import Mission from "path/to/components/Mission";
 import Team from "path/to/components/Team";
 import Reviews from "path/to/components/Reviews";
@@ -24,20 +24,20 @@ export default function App() {
         <Link to="/about" >
           About
         </Link>
-        <Link to="/ptoducts">
+        <Link to="/products">
           Products
         </Link>
   </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<About />}>
+        <Route path="/about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
-          <Route path="rewiews" element={<Reviews/>}/>
+          <Route path="reviews" element={<Reviews/>}/>
         </Route>
         <Route path="/products" element={<Products />} />
         <Route path="/products/:productId" element={<ProductDetails/>} />
-        <Route path="*" elemt={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
         <Route path="/blog/:postId" element={<BlogPost/>}/>
       </Routes>
     </div>
